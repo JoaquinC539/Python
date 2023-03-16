7a2e69113c516a86879d4e5004a08ca6e9e96d26
JoaquinC539 <cjoaquinangel@gmail.com>
2023-03-16 10:55:08 -0600

    made a rock paper scissor game

---
+++ rps.py
@@ -0,0 +1,37 @@
+import sys
+import random
+from enum import Enum
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+print("")
+playerchoice = input(
+    'Enter... \n1 for Rock, \n2 for Paper,\n3 for Scissors:\n\n ')
+
+player = int(playerchoice)
+
+if player < 1 | player > 3:
+    sys.exit("You must enter 1, 2, 3")
+
+computerchoice = random.choice("123")
+computer = int(computerchoice)
+print("")
+print("You choose: "+str(RPS(player)).replace('RPS.', '') + ".")
+print("Python choose: "+str(RPS(computer)).replace('RPS.', '') + ".")
+print("")
+
+if player == 1 and computer == 3:
+    print("🎉You win!")
+elif player == 2 and computer == 1:
+    print("🎉You win!")
+elif player == 3 and computer == 2:
+    print("🎉You win!")
+elif player == computer:
+    print("😮 It is a tie!")
+else:
+    print("🐍 Python win!")
